286378bed0a60850eeeb7432c535d023c64196c2
Thalia Martinez <thali.angemartinez@gmail.com>
2024-08-18 21:44:48 -0300

    solucion-click
    Agregué stopPropagation para detener la propagación del evento

--- eventos.js
+++ eventos.js
@@ -1,7 +1,7 @@
 const div = document.querySelector("div");
 
-function alerta() {
+function alertaDiv() {
     alert("Hola! Soy el div");
 }
 
-div.addEventListener ("click", alerta);
+div.addEventListener ("click", alertaDiv);
